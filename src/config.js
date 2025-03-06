d5cbb8c788bf81abbb8aeec8376508aec2b5c6a1
Ayomide Akinola <drayfocus@gmail.com>
2025-03-06 18:39:44 +0100

    fix: sudden fails

--- src/config.js
+++ src/config.js
@@ -55,7 +55,12 @@ const generateConfig = async (templateConfigurations) => {
   // extract templateVariables
   let templateVariables = {};
   if (templateConfigurations.hasOwnProperty("templateVariables")) {
-    templateVariables = JSON.parse(templateConfigurations.templateVariables);
+    try {
+      templateVariables = JSON.parse(templateConfigurations.templateVariables);
+    } catch (error) {
+      console.error("Error parsing templateVariables:", error);
+      return []; // Return an empty array to prevent further processing
+    }
   }
   const templateConfig = { ...templateConfigurations, ...templateVariables };
 
@@ -72,10 +77,15 @@ const generateConfig = async (templateConfigurations) => {
         fs.readdir(
           path.join(__dirname, `../configs${folder !== "" ? folder : ""}`),
           (err, files) => {
-            resolve({
-              files,
-              folder,
-            });
+            if (err) {
+              console.error("Error reading directory:", err);
+              resolve({ files: [], folder }); // Resolve with an empty array to prevent crash
+            } else {
+              resolve({
+                files,
+                folder,
+              });
+            }
           },
         );
       });
@@ -84,141 +94,166 @@ const generateConfig = async (templateConfigurations) => {
     const files = [];
 
     // Get root files and folders
-    const rootFileAndFolders = await readFolderFiles();
-
-    /**
-     * Recursively traverses the `configs` directory, identifying and processing YAML configuration files
-     * within folders specified by the `configsToUse` parameter.
-     *
-     * @async
-     * @returns {Promise<void>} A promise that resolves when all files are processed.
-     */
-    const getAllConfigFiles = async () => {
+    try {
+      const rootFileAndFolders = await readFolderFiles();
       /**
-       * Recursive helper function to process files and folders.
+       * Recursively traverses the `configs` directory, identifying and processing YAML configuration files
+       * within folders specified by the `configsToUse` parameter.
        *
        * @async
-       * @param {string} item Filename or folder name being processed.
-       * @param {string} [folder=""] Current folder path.
-       * @returns {Promise<void>} A promise resolving when the item is processed.
+       * @returns {Promise<void>} A promise that resolves when all files are processed.
        */
-      const getFilesInFolders = async (item, folder = "") => {
-        const itemIsFolder = item.split(".").length === 1;
+      const getAllConfigFiles = async () => {
+        /**
+         * Recursive helper function to process files and folders.
+         *
+         * @async
+         * @param {string} item Filename or folder name being processed.
+         * @param {string} [folder=""] Current folder path.
+         * @returns {Promise<void>} A promise resolving when the item is processed.
+         */
+        const getFilesInFolders = async (item, folder = "") => {
+          const itemIsFolder = item.split(".").length === 1;
 
-        if (itemIsFolder) {
-          const folderName = `${folder === "" ? `/${item}` : `${folder}`}`;
+          if (itemIsFolder) {
+            const folderName = `${folder === "" ? `/${item}` : `${folder}`}`;
 
-          const useConfigFolder =
-            (folderName.includes(`extras-${templateConfig.serviceName}`) ||
-              configsToUse.includes(folderName.substring(1))) &&
-            !folderName.includes(".git");
+            const useConfigFolder =
+              (folderName.includes(`extras-${templateConfig.serviceName}`) ||
+                configsToUse.includes(folderName.substring(1))) &&
+              !folderName.includes(".git");
 
-          console.log("folder detected: " + folderName);
+            console.log("folder detected: " + folderName);
 
-          if (useConfigFolder) {
-            const fileFolder = await readFolderFiles(folderName);
+            if (useConfigFolder) {
+              try {
+                const fileFolder = await readFolderFiles(folderName);
 
-            const allPromises = [];
+                const allPromises = [];
 
-            fileFolder.files.forEach((subItem) => {
-              allPromises.push(
-                getFilesInFolders(subItem, fileFolder.folder + "/" + subItem),
-              );
-            });
+                fileFolder.files.forEach((subItem) => {
+                  allPromises.push(
+                    getFilesInFolders(
+                      subItem,
+                      fileFolder.folder + "/" + subItem,
+                    ),
+                  );
+                });
 
-            await Promise.all(allPromises);
+                await Promise.all(allPromises);
+              } catch (error) {
+                console.error(
+                  "Error reading files in folder:",
+                  folderName,
+                  error,
+                );
+                // Continue processing other folders
+              }
+            }
+          } else {
+            const fullFilePath = `${folder !== "" ? folder : item}`;
+
+            if (
+              fullFilePath.split(".")[1] === "yaml" ||
+              fullFilePath.split(".")[1] === "yml"
+            ) {
+              files.push(fullFilePath);
+            }
           }
-        } else {
-          const fullFilePath = `${folder !== "" ? folder : item}`;
+        };
 
-          if (
-            fullFilePath.split(".")[1] === "yaml" ||
-            fullFilePath.split(".")[1] === "yml"
-          ) {
-            files.push(fullFilePath);
-          }
+        const allPromises = [];
+        rootFileAndFolders.files.forEach((item) => {
+          allPromises.push(getFilesInFolders(item, ""));
+        });
+
+        try {
+          await Promise.all(allPromises);
+        } catch (error) {
+          console.error("Error processing files in folders:", error);
+          // Continue processing other files.
         }
       };
 
-      const allPromises = [];
-      rootFileAndFolders.files.forEach((item) => {
-        allPromises.push(getFilesInFolders(item, ""));
-      });
+      await getAllConfigFiles();
 
-      await Promise.all(allPromises);
-    };
-
-    await getAllConfigFiles();
-
-    /**
-     * Reads a file, performs template substitution based on `templateConfig`, and returns
-     * a configuration object.
-     *
-     * @param {string} file The relative path to the file within the `configs` directory.
-     * @returns {Promise<{type: string, content: string} | null>} A promise resolving to an object containing the
-     *                                                              configuration `type` and the processed `content` (YAML string),
-     *                                                              or `null` if there was an error reading the file.
-     */
-    const readAndMakeTemplate = (file) => {
-      return new Promise((resolve) => {
-        fs.readFile(
-          path.join(__dirname, `../configs/${file}`),
-          { encoding: "utf-8" },
-          function (err, data) {
-            if (!err) {
-              let stringValue = `${data}`;
-              for (const key in templateConfig) {
-                if (templateConfig.hasOwnProperty(key)) {
-                  const value = templateConfig[key];
-                  stringValue = replaceAll(
-                    stringValue,
-                    "${" + key + "}",
-                    value,
-                  );
+      /**
+       * Reads a file, performs template substitution based on `templateConfig`, and returns
+       * a configuration object.
+       *
+       * @param {string} file The relative path to the file within the `configs` directory.
+       * @returns {Promise<{type: string, content: string} | null>} A promise resolving to an object containing the
+       *                                                              configuration `type` and the processed `content` (YAML string),
+       *                                                              or `null` if there was an error reading the file.
+       */
+      const readAndMakeTemplate = (file) => {
+        return new Promise((resolve) => {
+          fs.readFile(
+            path.join(__dirname, `../configs/${file}`),
+            { encoding: "utf-8" },
+            function (err, data) {
+              if (!err) {
+                let stringValue = `${data}`;
+                for (const key in templateConfig) {
+                  if (templateConfig.hasOwnProperty(key)) {
+                    const value = templateConfig[key];
+                    stringValue = replaceAll(
+                      stringValue,
+                      "${" + key + "}",
+                      value,
+                    );
+                  }
                 }
+                const configType = file.split(".")[0].toLocaleLowerCase();
+
+                resolve({
+                  type: configType,
+                  content: stringValue,
+                });
+              } else {
+                console.error("Error reading file:", file, err);
+                resolve(null);
               }
-              const configType = file.split(".")[0].toLocaleLowerCase();
+            },
+          );
+        });
+      };
 
-              resolve({
-                type: configType,
-                content: stringValue,
-              });
-            } else {
-              console.log(err);
-              resolve(null);
-            }
-          },
-        );
-      });
-    };
+      /**
+       * Processes all identified configuration files, reads their content, applies the template,
+       * and populates the `allConfigs` array.
+       *
+       * @async
+       * @returns {Promise<void>} A promise that resolves when all files are processed.
+       */
+      const configForAllFiles = async () => {
+        const allPromises = [];
+        files.forEach(async (file) => {
+          allPromises.push(
+            readAndMakeTemplate(file)
+              .then((fileConfig) => {
+                if (fileConfig) {
+                  allConfigs.push(fileConfig);
+                }
+              })
+              .catch((err) => {
+                console.error("Error processing template for file:", file, err);
+              }),
+          );
+        });
+        try {
+          await Promise.all(allPromises);
+        } catch (error) {
+          console.error("Error processing all files:", error);
+        }
+        resolveMain(allConfigs);
+      };
 
-    /**
-     * Processes all identified configuration files, reads their content, applies the template,
-     * and populates the `allConfigs` array.
-     *
-     * @async
-     * @returns {Promise<void>} A promise that resolves when all files are processed.
-     */
-    const configForAllFiles = async () => {
-      const allPromises = [];
-      files.forEach(async (file) => {
-        allPromises.push(
-          readAndMakeTemplate(file)
-            .then((fileConfig) => {
-              if (fileConfig) {
-                allConfigs.push(fileConfig);
-              }
-            })
-            .catch((err) => {
-              console.log(err);
-            }),
-        );
-      });
-      await Promise.all(allPromises);
-      resolveMain(allConfigs);
-    };
-
-    await configForAllFiles();
+      await configForAllFiles();
+    } catch (error) {
+      console.error("Error during initial folder read:", error);
+      resolveMain([]); // Resolve with an empty array in case of initial error.
+    }
   });
 };
 
@@ -233,42 +268,52 @@ const generateConfig = async (templateConfigurations) => {
  * @throws {Error} If there's an error parsing the YAML string or interacting with the K8s API.
  */
 const applyConfiguration = async (yamlString) => {
-  const client = k8s.KubernetesObjectApi.makeApiClient(kc);
-  const specs = yaml.loadAll(yamlString);
-  const validSpecs = specs.filter((s) => s && s.kind && s.metadata);
-  const created = [];
-  for (const spec of validSpecs) {
-    spec.metadata = spec.metadata || {};
-    spec.metadata.annotations = spec.metadata.annotations || {};
-    delete spec.metadata.annotations[
-      "kubectl.kubernetes.io/last-applied-configuration"
-    ];
-    spec.metadata.annotations[
-      "kubectl.kubernetes.io/last-applied-configuration"
-    ] = JSON.stringify(spec);
-    try {
-      // try to get the resource, if it does not exist an error will be thrown and we will end up in the catch
-      // block.
-      await client.read(spec);
-      // we got the resource, so it exists, so patch it
-      //
-      // Note that this could fail if the spec refers to a custom resource. For custom resources you may need
-      // to specify a different patch merge strategy in the content-type header.
-      //
-      // See: https://github.com/kubernetes/kubernetes/issues/97423
-      const response = await client.patch(spec);
-      created.push(response.body);
-    } catch (e) {
-      // we did not get the resource, so it does not exist, so create it
+  try {
+    const client = k8s.KubernetesObjectApi.makeApiClient(kc);
+    const specs = yaml.loadAll(yamlString);
+    const validSpecs = specs.filter((s) => s && s.kind && s.metadata);
+    const created = [];
+    for (const spec of validSpecs) {
+      spec.metadata = spec.metadata || {};
+      spec.metadata.annotations = spec.metadata.annotations || {};
+      delete spec.metadata.annotations[
+        "kubectl.kubernetes.io/last-applied-configuration"
+      ];
+      spec.metadata.annotations[
+        "kubectl.kubernetes.io/last-applied-configuration"
+      ] = JSON.stringify(spec);
       try {
-        const response = await client.create(spec);
+        // try to get the resource, if it does not exist an error will be thrown and we will end up in the catch
+        // block.
+        await client.read(spec);
+        // we got the resource, so it exists, so patch it
+        //
+        // Note that this could fail if the spec refers to a custom resource. For custom resources you may need
+        // to specify a different patch merge strategy in the content-type header.
+        //
+        // See: https://github.com/kubernetes/kubernetes/issues/97423
+        const response = await client.patch(spec);
         created.push(response.body);
-      } catch (error) {
-        console.log(error);
+      } catch (e) {
+        // we did not get the resource, so it does not exist, so create it
+        try {
+          const response = await client.create(spec);
+          created.push(response.body);
+        } catch (error) {
+          console.error(
+            "Error creating resource:",
+            spec.kind,
+            spec.metadata.name,
+            error,
+          );
+        }
       }
     }
+    return created;
+  } catch (error) {
+    console.error("Error applying configuration:", error);
+    return []; // return empty array so the process doesn't break
   }
-  return created;
 };
 
 // create or update app configurations using the template configurations
@@ -285,11 +330,11 @@ const enviromentData = process.env;
  * @throws {Error} If there's an error generating configurations or applying them to the K8s cluster.
  */
 const applyAppTemplate = async (templateConfig) => {
-  const templateAndEnvironmentData = { ...templateConfig, ...enviromentData };
-
-  const processedConfigs = await generateConfig(templateAndEnvironmentData);
-
   try {
+    const templateAndEnvironmentData = { ...templateConfig, ...enviromentData };
+
+    const processedConfigs = await generateConfig(templateAndEnvironmentData);
+
     if (processedConfigs.length > 0) {
       // apply namespace first
 
@@ -297,7 +342,13 @@ const applyAppTemplate = async (templateConfig) => {
         return configData.type == "namespace";
       });
 
-      await applyConfiguration(namespaceConfig[0].content);
+      if (namespaceConfig.length > 0) {
+        try {
+          await applyConfiguration(namespaceConfig[0].content);
+        } catch (error) {
+          console.error("Error applying namespace configuration:", error);
+        }
+      }
 
       // apply other configurations
 
@@ -305,12 +356,20 @@ const applyAppTemplate = async (templateConfig) => {
         return configData.type != "namespace";
       });
 
-      otherConfig.forEach(async (configData) => {
-        await applyConfiguration(configData.content);
-      });
+      for (const configData of otherConfig) {
+        try {
+          await applyConfiguration(configData.content);
+        } catch (error) {
+          console.error(
+            "Error applying configuration:",
+            configData.type,
+            error,
+          );
+        }
+      }
     }
   } catch (error) {
-    console.log(error);
+    console.error("Error in applyAppTemplate:", error);
   }
 };
 
